117ba51c116231595bed08b61bebfab55b6ed423
ashikmahmud <ashikmamud187@gmail.com>
2022-04-22 05:41:03 +0600

    initilization of my nodejs mongodb projects it's blog post project

---
+++ src/Pages/CreatePost/CreatePost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreatePost = () => {
+  return <section id="create-post">CreatePost</section>;
+};
+
+export default CreatePost;
---
+++ src/Pages/ManagePost/ManagePost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManagePost = () => {
+  return <section id="manage-post">ManagePost</section>;
+};
+
+export default ManagePost;
