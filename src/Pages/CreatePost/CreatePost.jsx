0ceecccc5299b730b53345843759d288d77b9ca0
ashikmahmud <ashikmamud187@gmail.com>
2022-04-22 16:10:36 +0600

    Create a create post system of this application:

--- src/Pages/CreatePost/CreatePost.jsx
+++ src/Pages/CreatePost/CreatePost.jsx
@@ -5,7 +5,43 @@ const CreatePost = () => {
   return (
     <CreatePostContainer id="create-post">
       <div className="container">
-        <h1>Create Post</h1>
+        <form action="" className="form-wrapper">
+          <h1>Create Post</h1>
+          <div className="input-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="title"
+              name="title"
+              placeholder="Title"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="category">Category</label>
+            <input
+              type="text"
+              className="category"
+              name="category"
+              placeholder="Category"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="desc">Description</label>
+            <textarea
+              className="desc"
+              name="desc"
+              placeholder="Description"
+              rows="7"
+            ></textarea>
+          </div>
+          <div className="input-group">
+            <label htmlFor="url">Featured Image URL</label>
+            <input type="url" placeholder="URL" name="url" id="url" />
+          </div>
+          <div className="input-group">
+            <button className="btn btn-primary">Save Your Post</button>
+          </div>
+        </form>
       </div>
     </CreatePostContainer>
   );
@@ -14,6 +50,9 @@ const CreatePost = () => {
 const CreatePostContainer = styled.section`
   position: relative;
   padding: 2rem;
+  .form-wrapper {
+    width: 100%;
+  }
 `;
 
 export default CreatePost;
